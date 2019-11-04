73fd593a192cad9fb8e4a8d5459696bb4d31a3c8
Melon <guang.xing@liulishuo.com>
2019-11-04 14:04:06 +0800

    feat: add imagemin

---
+++ index.js
@@ -0,0 +1,38 @@
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+const fs = require('fs');
+
+function readFileList(path, filesList) {
+  const files = fs.readdirSync(path);
+  files.forEach(function(itm) {
+    const stat = fs.statSync(path + itm);
+    if (stat.isDirectory() && !['node_modules', '.git'].includes(itm)) {
+      readFileList(path + itm + '/', filesList);
+    } else {
+      const res = {
+        path,
+        file: path + itm,
+      };
+      if (/.(jpg|png)$/gm.test(itm)) filesList.push(res);
+    }
+  });
+}
+
+(async () => {
+  console.log('开始压缩');
+  const fileList = [];
+  readFileList('./', fileList);
+  for (item of fileList) {
+    await imagemin([item.file], {
+      destination: item.path,
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({
+          quality: [0.6, 0.8],
+        }),
+      ],
+    });
+  }
+  console.log('压缩完毕');
+})();
